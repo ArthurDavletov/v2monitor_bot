887a197c4bcf01b8c93c40b488bafdd57e1f63c0
Arthur <ar.davletov2013@gmail.com>
2025-08-02 21:10:48 +0200

    Added stats, some documentation

--- bot/modules/bot.py
+++ bot/modules/bot.py
@@ -1,16 +1,18 @@
+"""This module contains the main bot logic, including command handlers"""
 import os
 import logging
 
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from aiogram.utils.formatting import Text, Bold, Pre
 
 from modules.logger import get_logger
-from modules.v2ray import is_service_active
+from modules.v2ray import is_service_active, get_stats
 
 
 load_dotenv()
@@ -18,6 +20,7 @@ logger = get_logger(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
+API_SERVER = os.getenv("API_SERVER")
 
 if not BOT_TOKEN:
     logger.error("BOT_TOKEN is not set in the environment variables.")
@@ -25,8 +28,13 @@ if not BOT_TOKEN:
 if not ADMIN_ID:
     logger.error("ADMIN_ID is not set in the environment variables.")
     raise ValueError("ADMIN_ID is required to run the bot.")
+if not API_SERVER:
+    logger.warning("API_SERVER is not set in the environment variables! Stats may not work correctly.")
 
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
 
 del BOT_TOKEN, ADMIN_ID
@@ -36,7 +44,11 @@ del BOT_TOKEN, ADMIN_ID
 async def command_start_handler(message: Message) -> None:
     """This handler receives messages with `/start` command"""
     logger.info(f"Received /start command from {message.from_user.full_name} (ID: {message.from_user.id})")
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    content = Text(
+        "Hello, ",
+        Bold(message.from_user.full_name)
+    )
+    await message.answer(**content.as_kwargs())
 
 
 @dp.message(Command("status"))
@@ -51,9 +63,18 @@ async def command_status_handler(message: Message) -> None:
             text += f"The {service} service isn't active. ❌\n"
     await message.answer(text)
 
+
+@dp.message(Command("stats"))
+async def command_stats_handler(message: Message) -> None:
+    """This handler receives messages with `/stats` command"""
+    logger.info(f"Received /stats command from {message.from_user.full_name} (ID: {message.from_user.id})")
+    text = get_stats(API_SERVER)
+    content = Text(Pre(text))
+    await message.answer(**content.as_kwargs())
+
+
 async def main() -> None:
     """Main function to start the bot."""
-    logger.info("Starting bot...")
     await dp.start_polling(bot)
 
 
--- bot/modules/v2ray.py
+++ bot/modules/v2ray.py
@@ -1,20 +1,78 @@
 import subprocess
+import json
+
+from prettytable import PrettyTable
+
 from modules.logger import get_logger
 
 
 logger = get_logger(__name__)
 
 
+def humanize_size(size: int) -> str:
+    """Convert bytes to a human-readable format."""
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1024:
+            return f"{size:.2f} {unit}"
+        size /= 1024
+    return f"{size:.2f} PB"
+
 def is_service_active(service_name: str = "v2ray") -> bool:
     """Check if the service is active."""
     try:
         result = subprocess.run(
             ["systemctl", "is-active", service_name],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            capture_output=True,
             text=True
         )
         return result.stdout.strip() == "active"
     except Exception as e:
         logger.error(f"Error checking service status: {e}")
         return False
+
+
+def get_stats(server: str) -> str:
+    """Get statistics of the v2ray service."""
+    try:
+        result = subprocess.run(
+            [
+                "v2ray", "api", "stats",
+                f"--server={server}",
+                "--json"
+            ],
+            capture_output=True,
+            text=True
+        )
+        if result.returncode != 0:
+            logger.error(f"Error getting stats: {result.stderr.strip()}.")
+            return "Error retrieving stats."
+        # Parse the JSON output
+        stats = []
+        for line in json.loads(result.stdout.strip()).get("stat", {}):
+            name = line.get("name", "")
+            if not name:
+                continue
+            direction, target, _, type = name.split(">>>")
+            stats.append({
+                "direction": direction.strip(),
+                "target": target.strip(),
+                "type": type.strip(),
+                "value": line.get("value", 0)
+            })
+        if not stats:
+            return "No statistics available."
+        stats.sort(key=lambda x: (x.get("direction", ""), x.get("target", ""), x.get("type", "")))
+        # Create a table to display the stats
+        table = PrettyTable()
+        table.field_names = ["Direction", "Target", "Type", "Value"]
+        for item in stats:
+            table.add_row([
+                item.get("direction", ""),
+                item.get("target", ""),
+                item.get("type", ""),
+                humanize_size(int(item.get("value", 0)))
+            ])
+        return str(table)
+    except Exception as e:
+        logger.error(f"Error retrieving stats: {e}")
+        return "Error retrieving stats."
